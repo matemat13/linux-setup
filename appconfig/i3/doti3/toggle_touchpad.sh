854be3323429976e6317cc87c96807619ec3fbf5
Matous <vrbamato@fel.cvut.cz>
2018-03-28 13:13:08 +0200

    commited Toshiba-specific changes

--- appconfig/i3/doti3/toggle_touchpad.sh
+++ appconfig/i3/doti3/toggle_touchpad.sh
@@ -3,7 +3,7 @@
 
 # run "xinput" to get this id, e.g. from the line:
 # ↳ DLL075B:01 06CB:76AF Touchpad             id=13   [slave  pointer  (2)]
-DEVICE_ID="06CB:76AF"
+DEVICE_ID="AlpsPS/2 ALPS DualPoint TouchPad"
 
 # find the id of my the touchpad
 DEVICE_NUMBER=`xinput | grep "$DEVICE_ID" | sed -r 's/.*id=([0-9]*).*/\1/g'`
